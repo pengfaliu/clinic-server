e22c2a176937f9268faba15dfc094d5390914f01
Yancysoul <zhengyong9709@163.com>
2019-12-26 09:49:56 +0800

    修改登陆验证

--- routes/disease.js
+++ routes/disease.js
@@ -1,9 +1,9 @@
 const router = require('koa-router')()
 const disease_controllers = require('../controllers/disease_controllers')
-const jwt_disease = require('../middleware/jwt_disease')
+const jwt_auto = require('../middleware/jwt_auto')
 
 router
-  .post('/diseasetype', jwt_disease, disease_controllers.disease_type)
+  .post('/diseasetype', jwt_auto, disease_controllers.disease_type)
   .post('/diseaselist', disease_controllers.disease_list)
 
 module.exports = router
